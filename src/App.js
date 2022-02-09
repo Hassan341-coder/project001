f3b3b59cbae606bf5535c2c69c91153486d39d05
Hassan <hassanqureshi341@gmail.com>
2022-02-09 20:15:24 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://Google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Go for search
+        </a>
+      </header>
+    <content>
+     this is content
+
+    </content>
+    
+    </div>
+      
+    
+);
+}
+
+export default App;
