5f899b903c42e75fb3043bbd8425728dc7480b44
Hassan <hassanqureshi341@gmail.com>
2022-02-14 00:12:08 +0500

    remove unused images and logo

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://Google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-            Hello World from Hassan
-        </a>
-      </header>
+     
+     <h1>Hello World this is my first commit</h1>
     <content>
      this is content
 
